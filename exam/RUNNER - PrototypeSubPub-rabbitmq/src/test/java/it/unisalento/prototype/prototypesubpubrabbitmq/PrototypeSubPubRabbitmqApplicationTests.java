40fa827045920a5e93ef0953bee48f3de6b35d2b
lonelygunter <aprile.matteo01@gmail.com>
2023-05-04 10:46:20 +0200

    moved the exam projects to the new "exams" folder

---
+++ exam/RUNNER - PrototypeSubPub-rabbitmq/src/test/java/it/unisalento/prototype/prototypesubpubrabbitmq/PrototypeSubPubRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unisalento.prototype.prototypesubpubrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototypeSubPubRabbitmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
